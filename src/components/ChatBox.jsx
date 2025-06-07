09b9899866ea151e8214f57d979f44d82d4a4e49
cMendoncaaa <cauamendooncaa55@gmail.com>
2025-06-07 14:42:45 -0300

    feat: Cria componente ChatBox para interface de chat

---
+++ src/components/ChatBox.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+const ChatBox = ({ onEnviarMensagem, desabilitado }) => {
+  const [mensagem, setMensagem] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onEnviarMensagem(mensagem);
+    setMensagem("");
+  };
+
+  return (
+    <div className="border-t border-gray-200 bg-gray-50/80 p-4">
+      <form className="flex space-x-3" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={mensagem}
+          onChange={(e) => setMensagem(e.target.value)}
+          placeholder="Digite o ingrediente ou uma receita que deseja aprender"
+          disabled={desabilitado}
+          className="flex-1 px-5 py-3 bg-white border border-gray-300 rounded-full shadow-sm placeholder-gray-400 focus:ring-2 outline-none focus:ring-purple-500"
+        />
+        <button
+          type="submit"
+          disabled={desabilitado}
+          className="px-8 py-3 bg-gradient-to-r from-purple-500 to-emerald-500 hover:from=purple-600 hover:to-emerald-700 cursor-pointer text-white rounded-full disabled:from-gray-400 disabled:to-gray-300 disabled:cursor-not-allowed"
+        >
+          Enviar
+        </button>
+      </form>
+    </div>
+  );
+};
+export default ChatBox;
